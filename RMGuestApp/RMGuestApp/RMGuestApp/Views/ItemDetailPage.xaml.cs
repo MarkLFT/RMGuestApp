bfd4da96d6e7028c54c4ee23396528d25ab55426
mark <mark@littlefishcom.net>
2017-03-23 16:57:06 +0800

    Initial Project Files

---
+++ RMGuestApp/RMGuestApp/RMGuestApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+using RMGuestApp.ViewModels;
+
+using Xamarin.Forms;
+
+namespace RMGuestApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        ItemDetailViewModel viewModel;
+
+        // Note - The Xamarin.Forms Previewer requires a default, parameterless constructor to render a page.
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+        }
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+    }
+}
